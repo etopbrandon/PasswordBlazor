7fd7438eea1dc5109658b691716b7b1f42efe6b7
etopbrandon <brandon@etoptechnology.com>
2022-06-15 10:51:42 -0700

    Add project files.

---
+++ PasswordBlazor/Data/ApiIntegration.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+
+namespace PasswordBlazor.Data
+{
+    public class ApiIntegration
+    {
+        
+        //UPDATE THESE!!!
+        //API Endpoint (usually your PWPush URL + "/p.json"):
+        public static string PwPushAPI = "https://password.etop.tech/p.json";
+        //PWPush Password URL (usually your PWPush URL + "/p/")
+        public static string PwPushPass = "https://password.etop.tech/p/";
+
+        //DONT TOUCH
+        public static readonly HttpClient client = new();
+    }
+
+    public class Root
+    {
+        public int? id { get; set; }
+        public string? payload { get; set; }
+        public int? expire_after_days { get; set; }
+        public int? expire_after_views { get; set; }
+        public bool? expired { get; set; }
+        public string? url_token { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public object? user_id { get; set; }
+        public bool deleted { get; set; }
+        public bool first_view { get; set; }
+        public bool deletable_by_viewer { get; set; }
+        public int days_remaining { get; set; }
+        public int views_remaining { get; set; }
+    }
+}
---
+++ PasswordBlazor/Data/Clipboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace PasswordBlazor.Data
+{
+    public class Clipboard
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public Clipboard(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask WriteTextAsync(string text)
+        {
+            return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
